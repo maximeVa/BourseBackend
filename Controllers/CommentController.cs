4034f56178da2bac153b05cbcbb5cf085dbe2197
maximeVa <maxime-v@live.be>
2025-05-25 17:56:54 +0200

    comment update

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -56,5 +56,17 @@ namespace api.Controllers
             await _commentRepository.CreateAsync(commentModel);
             return CreatedAtAction(nameof(GetById), new { id = commentModel.Id }, commentModel.ToCommentDto());
         }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateCommentRequestDto updateDto)
+        {
+            var comment = await _commentRepository.UpdateAsync(id, updateDto.ToCommentFromUpdate());
+            if (comment == null)
+            {
+                return NotFound("Comment not found.");
+            }
+            return Ok(comment.ToCommentDto());
+        }
     }
 }
--- Mappers/CommentMapper.cs
+++ Mappers/CommentMapper.cs
@@ -26,5 +26,14 @@ namespace api.Mappers
                 StockId = stockId
             };
         }
+
+        public static Comment ToCommentFromUpdate(this UpdateCommentRequestDto updateDto)
+        {
+            return new Comment
+            {
+                Title = updateDto.Title,
+                Content = updateDto.Content
+            };
+        }
     }
 }
