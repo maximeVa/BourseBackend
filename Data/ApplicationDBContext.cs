d9b33220374debe144b90d404eed01c80b3fce58
maximeVa <maxime-v@live.be>
2025-05-26 00:10:15 +0200

    identity parts - Jwt token bearer

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -1,13 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using api.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace api.Data
 {
-    public class ApplicationDBContext : DbContext
+    public class ApplicationDBContext : IdentityDbContext<AppUser>
     {
         public ApplicationDBContext(DbContextOptions dbContextOptions)
         : base(dbContextOptions)
